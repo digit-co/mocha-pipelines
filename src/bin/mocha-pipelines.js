76510008ff96ae0838eb9135a2a6213d90d18730
Sean Lavine <freewil@users.noreply.github.com>
2016-05-11 19:23:21 -0700

    default behavior: parallelize test across cpus

--- src/bin/mocha-pipelines.js
+++ src/bin/mocha-pipelines.js
@@ -7,35 +7,44 @@ const program = require('commander')
 const runPipeline = require('../lib/runPipeline')
 const pkg = require('../../package.json')
 
+let pipelines // total # of pipelines (VMs)
+let pipeline  // n of pipelines (VMs)
+let files     // test files to distribute across pipelines
+
 program
   .version(pkg.version)
   .option('-p, --processes [n]', 'specify number of processes to use', os.cpus().length)
   .arguments('<pipelines> <pipeline> [files...]')
-  .action((pipelines, pipeline, files) => {
-    let cpus = program.processes
-    debug(`Running action, pipelines: ${pipelines}, pipeline: ${pipeline}, files: ${files}, cpus: ${cpus}`)
-
-    // default test files to `test/` dir
-    if (!files.length) {
-      files = ['test/']
-    }
-
-    runPipeline(files, pipelines, pipeline, cpus, (err, exitCodes) => {
-      debug(`Mocha processes closed, err: ${err}, exitCodes: ${exitCodes}`)
-      if (err) {
-        console.error(`Unexpected error running mocha-pipelines: ${err.toString()}`)
-        return process.exit(1)
-      }
-
-      // mocha usually exits with an exit code equal to number of failed tests.
-      // so let's exit with the sum of those across all processes.
-      let exitCode = exitCodes.reduce((a, b) => a + b)
-      process.exit(exitCode)
-    })
+  .action((pipelinesArg, pipelineArg, filesArg) => {
+    pipelines = pipelinesArg
+    pipeline = pipelineArg
+    files = filesArg
   })
   .parse(process.argv)
 
-// output help if no arguments are given
-if (!process.argv.slice(2).length) {
-  program.outputHelp()
+let cpus = program.processes
+
+// default test files to `test/` dir
+if (!files || !files.length) {
+  files = ['test/']
 }
+
+// default behavior without any arguments: just parallelize test files across
+// available cpus
+pipelines = pipelines || 1
+pipeline = pipeline || 1
+
+debug(`pipelines: ${pipelines}, pipeline: ${pipeline}, files: ${files}, cpus: ${cpus}`)
+
+runPipeline(files, pipelines, pipeline, cpus, (err, exitCodes) => {
+  debug(`Mocha processes closed, err: ${err}, exitCodes: ${exitCodes}`)
+  if (err) {
+    console.error(`Unexpected error running mocha-pipelines: ${err.toString()}`)
+    return process.exit(1)
+  }
+
+  // mocha usually exits with an exit code equal to number of failed tests.
+  // so let's exit with the sum of those across all processes.
+  let exitCode = exitCodes.reduce((a, b) => a + b)
+  process.exit(exitCode)
+})
